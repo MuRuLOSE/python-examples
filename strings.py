9979bb274c9ec6509fc310342767edb82a538593
Macsim <podryazhchik@mail.ru>
2023-09-30 12:55:53 +0300

    added strings

---
+++ strings.py
@@ -0,0 +1,18 @@
+# Strings...
+
+# You cant modify strings, but can replace letters
+
+a = "Hello world" # string type variable
+
+print(a) # Print variable
+print("Hello World!") # print string
+
+b = "Some string..."
+
+print(b.capitalize()) # The first letter is big, the rest are small
+
+print(b.lower()) # All letters lower
+
+print(b.title()) # Like capitalize
+
+print(b.upper()) # All letters caps-lock
